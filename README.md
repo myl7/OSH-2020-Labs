0c033c6166b2ad465ab7e1f8d793651b7f5502b9
myl7 <myl.ustc@gmail.com>
2020-04-04 19:27:39 +0800

    [lab2] Init project.
    Add required deps.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Labs of USTC *Operating Systems (H) 2020 Spring*.
 ## Reports
 
 - [Lab1](./lab1/docs/README.md)
+- [Lab2](./lab2/README.md)
---
+++ lab2/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate dirs;
+extern crate regex;
+#[allow(unused_imports)]
+#[macro_use]
+extern crate lazy_static;
+#[allow(unused_imports)]
+#[macro_use]
+extern crate quick_error;
