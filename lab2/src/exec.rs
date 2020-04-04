d1996ba5e3e82bd465caec8ae7972ce43f0f4bca
myl7 <myl.ustc@gmail.com>
2020-04-05 06:26:51 +0800

    [lab2] Add kill support and ctrl-c handle. Add input prompt.

--- lab2/src/exec.rs
+++ lab2/src/exec.rs
@@ -1,7 +1,25 @@
 use crate::parse::{StdinType, StdoutMode, StdoutType};
 use crate::{Cmd, Result};
+use libc::{kill, pid_t, SIGINT};
+use std::cell::RefCell;
 use std::fs::{File, OpenOptions};
 use std::process::{Child, Command, Stdio};
+
+thread_local! {
+    static EXEC_PIDS: RefCell<Vec<u32>> = RefCell::new(Vec::new());
+}
+
+pub fn clear_exec_pids() {
+    EXEC_PIDS.with(|c| {
+        for pid in c.borrow().iter() {
+            unsafe {
+                kill(*pid as pid_t, SIGINT);
+            }
+        }
+
+        c.borrow_mut().clear();
+    });
+}
 
 impl Cmd {
     pub fn execute(&self) -> Result<i32> {
@@ -14,8 +32,8 @@ impl Cmd {
                 &[]
             };
 
-            piped = Some(
-                Command::new(prog)
+            piped = Some({
+                let child = Command::new(prog)
                     .args(args)
                     .stdin(match &sub_cmd.stdin {
                         StdinType::Inherit => Stdio::inherit(),
@@ -45,8 +63,15 @@ impl Cmd {
                         },
                     })
                     .spawn()
-                    .map_err(|_| "Execution failed.")?,
-            )
+                    .map_err(|_| "Execution failed.")?;
+
+                // Save pid.
+                EXEC_PIDS.with(|c| {
+                    c.borrow_mut().push(child.id());
+                });
+
+                child
+            })
         }
 
         let exit_status = piped.unwrap().wait().map_err(|e| e.to_string())?;
--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -1,8 +1,20 @@
-use msh::{Cmd, Error};
-use std::process;
+use msh::{exec, Cmd, Error};
+use std::io::Write;
+use std::{io, process};
 
 fn main() -> ! {
+    ctrlc::set_handler(move || {
+        exec::clear_exec_pids();
+
+        print!("\n# ");
+        io::stdout().flush().expect("Fail to Flush stdout.");
+    })
+    .expect("Can not handle SIGINT which can be caused by Ctrl+C.");
+
     loop {
+        print!("# ");
+        io::stdout().flush().expect("Fail to Flush stdout.");
+
         match Cmd::new() {
             Err(e) => match e {
                 Error::ReadFailed => process::exit(-1),
