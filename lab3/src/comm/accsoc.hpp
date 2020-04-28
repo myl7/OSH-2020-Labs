0c73b1f02105dcaf493c1ad448905d4714efc881
myl7 <myl.ustc@gmail.com>
2020-04-28 16:17:25 +0800

    [lab3] Multi-thread edition demo finished.

---
+++ lab3/src/comm/accsoc.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <stdexcept>
+
+extern "C" {
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+}
+
+constexpr int QUEUED_CON_N = 3;
+
+class AccSoc {
+public:
+  explicit AccSoc(in_port_t port) {
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+      throw std::runtime_error("Can not create a new socket.");
+    }
+
+    sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(port),
+        .sin_addr = {.s_addr = INADDR_ANY},
+    };
+
+    if (bind(fd, (sockaddr *)&addr, sizeof(addr))) {
+      throw std::runtime_error("Can not bind the socket to port " + std::to_string(port) + ".");
+    }
+
+    if (listen(fd, QUEUED_CON_N)) {
+      throw std::runtime_error("Can not listen " + std::to_string(QUEUED_CON_N) + " connections on the socket.");
+    }
+  }
+
+  ~AccSoc() {
+    close(fd);
+  }
+
+  int accept() {
+    auto serve_fd = ::accept(fd, nullptr, nullptr);
+
+    if (serve_fd == -1) {
+      throw std::runtime_error("Can not accept a connection on the socket.");
+    }
+
+    return serve_fd;
+  }
+
+private:
+  int fd;
+};
--- lab3/src/mt/main.cpp
+++ lab3/src/mt/main.cpp
@@ -1,9 +1,105 @@
 #include <iostream>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <queue>
+
+#include <comm/accsoc.hpp>
+#include <comm/srvsoc.hpp>
+
+constexpr in_port_t port = 8000;
+constexpr int CON_N = 32;
+
+int fd_list[CON_N] = {};
+std::mutex *mtx_list[CON_N];
+std::condition_variable *cv_list[CON_N];
+std::thread recv_thds[CON_N];
+std::thread send_thds[CON_N];
+
+void init() {
+  for (int i = 0; i < CON_N; ++i) {
+    mtx_list[i] = new std::mutex();
+    cv_list[i] = new std::condition_variable();
+  }
+}
+
+void clean() {
+  for (int i = 0; i < CON_N; ++i) {
+    delete mtx_list[i];
+    delete cv_list[i];
+  }
+}
 
 int main() {
   std::ios::sync_with_stdio(false);
 
-  std::cout << "Hello, World!" << std::endl;
+  AccSoc accsoc(port);
+
+  init();
+
+  std::queue<std::string> q_list[CON_N] = {{}};
+
+  for (int i = 0; i < CON_N; ++i) {
+    fd_list[i] = accsoc.accept();
+
+    std::cerr << "Accept: " << fd_list[i] << std::endl;
+
+    // Send thread.
+    send_thds[i] = std::move(std::thread([&, i] {
+      while (true) {
+        ssize_t len;
+        std::string msg{};
+
+        do {
+          {
+            std::unique_lock lock(*mtx_list[i]);
+            cv_list[i]->wait(lock, [&] {return !q_list[i].empty();});
+            msg = q_list[i].front();
+            q_list[i].pop();
+          }
+
+          std::cerr << "Send: " << msg << std::endl;
+
+          len = send_msg(msg + "\n", fd_list[i]);
+        } while (len > 0);
+      }
+    }));
+
+    // Recv thread.
+    recv_thds[i] = std::move(std::thread([&, i] {
+      while (true) {
+        ssize_t len;
+        std::string msg;
+
+        while (std::tie(len, msg) = recv_msg(fd_list[i]), len > 0) {
+          std::cerr << "Recv: " << msg << std::endl;
+
+          for (int j = 0; j < CON_N; ++j) {
+            if (j != i && fd_list[j] != 0) {
+              {
+                std::lock_guard lock(*mtx_list[j]);
+                q_list[j].push(msg);
+              }
+
+              cv_list[j]->notify_one();
+            }
+          }
+        }
+
+        close(fd_list[i]);
+        fd_list[i] = 0;
+        fd_list[i] = accsoc.accept();
+      }
+    }));
+  }
+
+  for (int i = 0; i < CON_N; ++i) {
+    recv_thds[i].join();
+    send_thds[i].join();
+  }
+
+  clean();
 
   return 0;
 }
