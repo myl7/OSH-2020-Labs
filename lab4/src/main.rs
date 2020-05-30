7611af1c0ec9d339db2309b4d1162c94bce5899a
myl7 <myl.ustc@gmail.com>
2020-05-30 18:16:33 +0800

    [lab4] Add cli command part.

--- lab4/src/main.rs
+++ lab4/src/main.rs
@@ -9,23 +9,31 @@ pub mod consts;
 pub mod host;
 
 use nix::unistd::Pid;
-use std::env::current_dir;
-use std::fs::{read_dir, File};
-use std::io::Read;
+use std::env::args;
+use std::process::Command;
 
 fn main() {
+    let mut args = args().collect::<Vec<String>>();
+
+    if args.len() < 3 {
+        panic!("Too few arguments.");
+    }
+
+    args.remove(0);
+
+    let root_s = args.remove(0);
+    let prog = args.remove(0);
+
     let (fd1, fd2) = host::mkpipe();
 
     let tmpdir = host::mktmpdir();
-    let tmpdir_cntr = tmpdir.clone();
+    let tmpdir_s = tmpdir.clone();
 
     host::create_cgroup();
 
     let child = Box::new(move || {
-        let tmpdir = tmpdir_cntr.as_str();
-
-        let cwd = current_dir().unwrap();
-        let root = cwd.to_str().unwrap();
+        let root = root_s.as_str();
+        let tmpdir = tmpdir_s.as_str();
 
         cntr::mount_all(root, tmpdir);
         cntr::mknod_all(root);
@@ -35,17 +43,20 @@ fn main() {
         cntr::limit_caps();
         cntr::limit_syscall();
 
-        let files = read_dir("/")
-            .unwrap()
-            .map(|p| p.unwrap().path().to_str().unwrap().to_string())
-            .collect::<Vec<String>>();
+        // Debug code
+        // let files = read_dir("/")
+        //     .unwrap()
+        //     .map(|p| p.unwrap().path().to_str().unwrap().to_string())
+        //     .collect::<Vec<String>>();
+        //
+        // println!("{:?}", files);
+        //
+        // let mut s = String::new();
+        // let mut f = File::open("/proc/1/cgroup").unwrap();
+        // f.read_to_string(&mut s).unwrap();
+        // println!("{}", &s);
 
-        println!("{:?}", files);
-
-        let mut s = String::new();
-        let mut f = File::open("/proc/1/cgroup").unwrap();
-        f.read_to_string(&mut s).unwrap();
-        println!("{}", &s);
+        Command::new(&prog).args(&args).status().unwrap();
 
         0
     });
